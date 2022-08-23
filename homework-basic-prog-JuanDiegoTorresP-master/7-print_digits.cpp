7f520cd7f12d07486e0cef4753e1b5a494e5f672
JuanDiegoTorresP <110799476+JuanDiegoTorresP@users.noreply.github.com>
2022-08-22 21:50:33 -0500

    Update 7-print_digits.cpp

--- homework-basic-prog-JuanDiegoTorresP-master/7-print_digits.cpp
+++ homework-basic-prog-JuanDiegoTorresP-master/7-print_digits.cpp
@@ -1,16 +1,23 @@
    #include <iostream>
    #include <cstdlib>
 
-   void print_digits(int num);
+   void print_digits(int a,int b,int c, int d);
 
    int main(int argc, char *argv[]) {
-     int a = std::atoi(argv[1]);
-
-     print_digits(a);
-
-     return 0;
+      int a=0, b=0, c=0, d=0;
+     int num = std::atoi(argv[1]);
+if(num>=0&&num<10000){
+   
+   a=num%10;
+   b=num%100-a;
+   c=num%1000-a-b;
+   d=num-a-b-c;
    }
-      void print_digits(int num)
-      {
-        TODO: Remove this line and complete the function
+      else{
+         std::cout<<''Numero invalido''<<std::endl;
+   }
+     print_digits(a, b, c, d);
+   }
+      void print_digits(int a, int b, int c, int d){
+         std::cout<<d/1000<<'' ''<<c/100'' ''<<b/10<<'' ''a'std::endl;
       }
