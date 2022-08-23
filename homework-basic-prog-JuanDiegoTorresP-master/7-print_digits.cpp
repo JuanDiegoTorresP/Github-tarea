d1aab8f9f236f29dbecb3b741d2edd874424483d
JuanDiegoTorresP <110799476+JuanDiegoTorresP@users.noreply.github.com>
2022-08-22 22:17:01 -0500

    Update 7-print_digits.cpp

--- homework-basic-prog-JuanDiegoTorresP-master/7-print_digits.cpp
+++ homework-basic-prog-JuanDiegoTorresP-master/7-print_digits.cpp
@@ -1,23 +1,27 @@
-   #include <iostream>
-   #include <cstdlib>
+// 7-print_digits.cpp
+#include <iostream>
+#include <cstdlib>
 
-   void print_digits(int a,int b,int c, int d);
+void print_digits(int a,int b,int c,int d);
 
-   int main(int argc, char *argv[]) {
-      int a=0, b=0, c=0, d=0;
-     int num = std::atoi(argv[1]);
-if(num>=0&&num<10000){
-   
-   a=num%10;
-   b=num%100-a;
-   c=num%1000-a-b;
-   d=num-a-b-c;
-   }
-      else{
-         std::cout<<"Numero invalido"<<std::endl;
-   }
-     print_digits(a, b, c, d);
-   }
-      void print_digits(int a, int b, int c, int d){
-         std::cout<<d/1000<<" "<<c/100" "<<b/10<<" "a"std::endl;
-      }
+
+int main(int argc, char *argv[]) {
+  int a, b,c,d;
+  
+  int num = std::atoi(argv[1]);
+
+  if(num>=0&&num<10000){
+    a=num%10; //Unidades
+    b=num%100-a; //Decenas
+    c=num%1000-a-b; //Centenas
+    d=num-a-b-c; //Milenas
+    print_digits(a,b,c,d);
+  }
+  else{
+    std::cout<<"Numero invalido"<<std::endl;
+  }
+}
+
+void print_digits(int a,int b,int c,int d){
+  std::cout<<d/1000<<" "<<c/100<<" "<<b/10<<" "<<a<<std::endl;
+}
