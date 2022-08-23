0b5ef0eca7e4b42fa661b981dbf3512c92b97b33
JuanDiegoTorresP <110799476+JuanDiegoTorresP@users.noreply.github.com>
2022-08-22 22:17:33 -0500

    Update 10-prime.cpp

--- homework-basic-prog-JuanDiegoTorresP-master/10-prime.cpp
+++ homework-basic-prog-JuanDiegoTorresP-master/10-prime.cpp
@@ -1,18 +1,30 @@
- #include <iostream>
- #include <cstdlib>
- #include <cmath>
-
- bool isprime(int n);
-
- int main(int argc, char *argv[]) {
-   int m = std::atoi(argv[1]);
-
-   std::cout << isprime(m) << "\n";
-
-   return 0;
- }
-
- bool isprime(int n)
- {
-   TODO: Elimine esta linea e implemente la solucion
- }
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+bool isprime(int n);
+int main(int argc,char*argv[]){
+	int m=atoi(argv[1]);
+	cout<<"digite un numero"<<endl;
+	cin>>m;
+	if(isprime(m)){
+		cout<<"es primo";
+	}
+	else{
+		cout<<"no es primo"<<endl;
+	}
+	cout<<isprime(m)<<"\n";
+	
+	return 0;
+}
+bool isprime(int n){
+	if(n==0||n==1||n==4){
+		return false;
+		for(int i=2;i<n/2;i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+	}
+	return true;
+}
